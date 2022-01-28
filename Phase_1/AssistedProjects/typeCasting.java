bba8d8b544fef4192b551ad3eee6bec0a7679be8
VikramGowda <90854599+VikramGowda@users.noreply.github.com>
2022-01-28 14:12:02 +0530

    Add files via upload

---
+++ Phase_1/AssistedProjects/typeCasting.java
@@ -0,0 +1,38 @@
+package Code;
+
+public class typeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+				//implicit conversion
+				System.out.println("Implicit Type Casting:");
+				char a = 'V';
+				System.out.println("Value of a: "+a);
+				
+				int b = a;
+				System.out.println("Value of b: "+b);
+				
+				float c = a;
+				System.out.println("Value of c: "+c);
+				
+				long d = a;
+				System.out.println("Value of d: "+d);
+				
+				double e = a;
+				System.out.println("Value of e: "+e);
+				
+				
+				System.out.println("Explicit Type Casting:");
+				//explicit conversion
+				
+				int y = 21;
+				double z = (double)y;
+				System.out.println("Value of x: "+y);
+				System.out.println("Value of y: "+z);
+				
+			}
+
+
+	}
+
