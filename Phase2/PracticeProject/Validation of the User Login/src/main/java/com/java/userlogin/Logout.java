bbb858fdb7021c7c0585929297035483db6b16dc
VikramGowda <90854599+VikramGowda@users.noreply.github.com>
2022-02-12 13:10:53 +0530

    Add files via upload

---
+++ Phase2/PracticeProject/Validation of the User Login/src/main/java/com/java/userlogin/Logout.java
@@ -0,0 +1,29 @@
+package com.java.userlogin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+//		Invalidate the session here
+		request.getSession().invalidate();
+		
+		out.println("<h2>You are now logged out...<br>");
+		
+		out.println("<a href='index.html'>Login again</a>");
+	}
+
+}
